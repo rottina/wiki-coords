4db45b827a68bd23f25d76f15736fdf8b0ec4c52
Mike Rottina <rottinam@gmail.com>
2020-11-20 19:12:44 -0500

    Update to match Goole map right-click behavior

--- wiki.coords.js
+++ wiki.coords.js
@@ -1,12 +1,13 @@
 javascript: (function() {
-    const MSG = 'Please manually update the "type:" value.\nIf outside the U.S., update the "earth_region:" value.\n\nSome common types:\nadm1st adm2nd adm3rd airport city edu event forest isle landmark mountain railwaystation river waterbody';
-    const region = 'US-';
-    const type = 'landmark';
-    let coords = document.getElementsByClassName('action-menu-entry-text')[0];
+    const msg = "Please manually update the \"type:\" value.\nIf outside the U.S., update the \"earth_region:\" value.\n\nSome common types:\nadm1st adm2nd adm3rd airport city edu event forest isle landmark mountain railwaystation river waterbody";
+    const comma = ",";
+    const region = "US-";
+    const type = "landmark";
+    let coords = document.getElementsByClassName("action-menu-entry-text")[0];
     if (coords === undefined) {
-        coords = 'ADD_COORDS'
+        coords = "ADD_COORDS";
     } else {
-        coords = coords.innerHTML.replace(', ', '|');
+        coords = coords.innerHTML.replace(comma, "|");
     }
-    prompt(MSG, "{{coord|" + coords + "|type:" + type + "_globe:earth_region:" + region + "|display=title}}")
+    prompt(msg, "{{coord|" + coords + "|type:" + type + "_globe:earth_region:" + region + "|display=title}}")
 })()
