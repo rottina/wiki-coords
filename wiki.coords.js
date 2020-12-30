2aa5984ed3c20044c0833495fb4b87eb2e162d91
Mike Rottina <rottinam@gmail.com>
2020-12-30 13:29:07 -0500

    Update comma replace

--- wiki.coords.js
+++ wiki.coords.js
@@ -1,13 +1,13 @@
 javascript: (function() {
-    const msg = "Please manually update the \"type:\" value.\nIf outside the U.S., update the \"earth_region:\" value.\n\nSome common types:\nadm1st adm2nd adm3rd airport city edu event forest isle landmark mountain railwaystation river waterbody";
-    const comma = ",";
-    const region = "US-";
-    const type = "landmark";
-    let coords = document.getElementsByClassName("action-menu-entry-text")[0];
-    if (coords === undefined) {
-        coords = "ADD_COORDS";
-    } else {
-        coords = coords.innerHTML.replace(comma, "|");
-    }
-    prompt(msg, "{{coord|" + coords + "|type:" + type + "_globe:earth_region:" + region + "|display=title}}")
+  const msg = "Please manually update the \"type:\" value.\nIf outside the U.S., update the \"earth_region:\" value.\n\nSome common types:\nadm1st adm2nd adm3rd airport city edu event forest isle landmark mountain railwaystation river waterbody";
+  const comma = ", ";
+  const region = "US-";
+  const type = "landmark";
+  let coords = document.getElementsByClassName("action-menu-entry-text")[0];
+  if (coords === undefined) {
+      coords = "ADD_COORDS";
+  } else {
+      coords = coords.innerText.replace(comma, "|");
+  }
+  prompt(msg, "{{coord|" + coords + "|type:" + type + "_globe:earth_region:" + region + "|display=title}}")
 })()
