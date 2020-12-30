5cfa37398667cd4dd5f9fe0146158704b61ea13c
Mike Rottina <rottinam@gmail.com>
2020-12-30 13:32:00 -0500

    Update style

--- wiki.coords.js
+++ wiki.coords.js
@@ -1,13 +1,14 @@
 javascript: (function() {
-  const msg = "Please manually update the \"type:\" value.\nIf outside the U.S., update the \"earth_region:\" value.\n\nSome common types:\nadm1st adm2nd adm3rd airport city edu event forest isle landmark mountain railwaystation river waterbody";
   const comma = ", ";
+  const gmapClass = "action-menu-entry-text";
+  const msg = "Please manually update the \"type:\" value.\nIf outside the U.S., update the \"earth_region:\" value.\n\nSome common types:\nadm1st adm2nd adm3rd airport city edu event forest isle landmark mountain railwaystation river waterbody";
   const region = "US-";
   const type = "landmark";
-  let coords = document.getElementsByClassName("action-menu-entry-text")[0];
+  let coords = document.getElementsByClassName(gmapClass)[0];
   if (coords === undefined) {
-      coords = "ADD_COORDS";
+    coords = "ADD_COORDS";
   } else {
-      coords = coords.innerText.replace(comma, "|");
+    coords = coords.innerText.replace(comma, "|");
   }
   prompt(msg, "{{coord|" + coords + "|type:" + type + "_globe:earth_region:" + region + "|display=title}}")
 })()
