343dff39814b0b81eed408ea528a0515b1bd104a
Mike Rottina <rottinam@gmail.com>
2022-11-26 20:59:23 -0500

    Update selector

--- wiki.coords.js
+++ wiki.coords.js
@@ -1,13 +1,13 @@
-javascript: (function() {
-  const comma = ", ";
-  const msg = "Please manually update the \"type:\" value.\nIf outside the U.S., update the \"earth_region:\" value.\n\nSome common types:\nadm1st adm2nd adm3rd airport city edu event forest isle landmark mountain railwaystation river waterbody";
+javascript: (() => {
   const region = "US-";
+  const comma = ", ";
   const type = "landmark";
-  let coords = document.querySelector("#action-menu > ul > li:nth-child(1) > div.nbpPqf-menu-x3Eknd-text-haAclf > div.nbpPqf-menu-x3Eknd-text");
+  const msg = "Please manually update the \"type:\" value.\nIf outside the U.S., update the \"earth_region:\" value.\n\nSome common types:\nadm1st adm2nd adm3rd airport city edu event forest isle landmark mountain railwaystation river waterbody";
+  let coords = document.querySelector("#action-menu > div:nth-child(1) > div > div");
   if (coords === undefined) {
     coords = "ADD_COORDS";
   } else {
     coords = coords.innerText.replace(comma, "|");
   }
   prompt(msg, "{{coord|" + coords + "|type:" + type + "_globe:earth_region:" + region + "|display=title}}")
-})()
+})();
